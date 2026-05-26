c5969d7f944495c8610825d3e2c3d36d436d913e
Flávia P. Pissarra <66925367+fppissarra@users.noreply.github.com>
2026-05-26 20:40:20 -0300

    Create dictionary.js

---
+++ core/data/dictionary.js
@@ -0,0 +1,19 @@
+export const dictionary = {
+    "welcome.title": {
+        en: "Welcome",
+        "pt-BR": "Bem-vindo",
+        "es-419": "Bienvenido",
+        it: "Benvenuto",
+        ja: "歓迎",
+        ko: "환영합니다"
+    },
+    "nav.about": {
+        en: "About Me",
+        "pt-BR": "Sobre Mim",
+        "es-419": "Sobre Mí",
+        it: "Su di Me",
+        ja: "自己紹介",
+        ko: "소개"
+    }
+    // New keys will be added strictly here in English format
+};
